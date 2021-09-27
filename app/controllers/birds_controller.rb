cdc4c6924e4c59051d896517cbcc555da6da9a59
Lena Hou <lenahou2013@gmail.com>
2021-09-27 11:39:44 -0400

    commit tree 4cbb639e7c4cf5e21a614fe1348d4448461b181c

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,10 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    render json: birds
   end
 end
+
+# we only need instance variables when we are rendering to ERB. 
+# Now that we are directly rendering to JSON in the same action, we no longer need to deal with instance variables 
+# and can instead just use a local variable:birds
